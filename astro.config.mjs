d0256f677fb22bcd61b77a77defbdf92f5953fae
Gek Siong Low <geksiong@yahoo.com>
2022-08-24 12:43:48 +0800

    fix: Markdown plugins not working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,41 +1,33 @@
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
+import { defineConfig } from 'astro/config';
 
-// @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ ({
+export default defineConfig ({
   site: 'https://geksiong.github.io',
 	integrations: [],
   markdown: {
-    render: [
-      '@astrojs/markdown-remark',
-      {
-        remarkPlugins: [
-          // Add a Remark plugin that you want to enable for your project.
-          // If you need to provide options for the plugin, you can use an array and put the options as the second item.
-          // ['remark-autolink-headings', { behavior: 'prepend'}],
-          [
-            'remark-math',
-            'remark-gfm',
-            'remark-smartypants',
-          ],
-        ],
-        rehypePlugins: [
-          // Add a Rehype plugin that you want to enable for your project.
-          // If you need to provide options for the plugin, you can use an array and put the options as the second item.
-          'rehype-slug',
-          //['rehype-autolink-headings', { behavior: 'prepend'}],
-          ['rehype-katex', {} ],
-        ],
-        syntaxHighlight: 'shiki',
-        // If you are using shiki, here you can define a global theme and
-        // add custom languages.
-        shikiConfig: {
-          theme: 'dracula-soft',
-          langs: [ ],
-          wrap: false,
-        },
-      },
+    remarkPlugins: [
+      // Add a Remark plugin that you want to enable for your project.
+      // If you need to provide options for the plugin, you can use an array and put the options as the second item.
+      // ['remark-autolink-headings', { behavior: 'prepend'}],
+      'remark-math',
+      'remark-gfm',
+      'remark-smartypants',
     ],
+    rehypePlugins: [
+      // Add a Rehype plugin that you want to enable for your project.
+      // If you need to provide options for the plugin, you can use an array and put the options as the second item.
+      //['rehype-autolink-headings', { behavior: 'prepend'}],
+      ['rehype-katex', {} ],
+    ],
+    syntaxHighlight: 'shiki',
+    // If you are using shiki, here you can define a global theme and
+    // add custom languages.
+    shikiConfig: {
+      theme: 'dracula-soft',
+      langs: [ ],
+      wrap: false,
+    }
   },
 	vite: {
     ssr: {
